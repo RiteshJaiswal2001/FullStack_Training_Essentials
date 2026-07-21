ca83f83cdd956f6b08cb062e2c68c8a05801f3cb
RiteshJaiswal2001 <rkjais10022001@gmail.com>
2026-07-21 11:52:44 +0530

    MultiThread & Thread Poll

---
+++ day_04/thread_pool.java
@@ -0,0 +1,20 @@
+package day_04;
+
+
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class thread_pool {
+    public static void main(String[] args) {
+        ExecutorService exec = Executors.newFixedThreadPool(3);
+
+        for (int i = 0; i < 5; i++) {
+            exec.execute(() -> {
+                System.out.println("Async task running: " + Thread.currentThread().getName());
+            });
+        }
+
+        exec.shutdown();
+    }
+}
